5fb8a4ba2728701cff345b3b23638260b2350f5d
Tejas Cousik <tcousik0@gmail.com>
2020-05-26 20:59:58 -0600

    first

---
+++ src/form.js
@@ -0,0 +1,39 @@
+import React from "react";
+import shortid from "shortid";
+
+export default class Form extends React.Component {
+  state = {
+    text: "",
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.addTodo({
+      text: this.state.text,
+      id: shortid.generate(),
+    });
+    this.setState({
+      text: "",
+    });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          name="text"
+          value={this.state.text}
+          onChange={this.handleChange}
+          required
+        ></input>
+        <input type="submit"></input>
+      </form>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import Form from "./form";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export default class TodoList extends React.Component {
+  state = {
+    todos: [],
+  };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  addTodo = (todo) => {
+    this.setState({
+      todos: [todo, ...this.state.todos],
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Form addTodo={this.addTodo} />
+        {this.state.todos.map((todo) => (
+          <div id={todo.id}>
+            <h2>{todo.text}</h2>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<TodoList />, document.getElementById("root"));
