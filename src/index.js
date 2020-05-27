d352303762112a86ff1b547ba81137d4597b37af
Tejas Cousik <tcousik0@gmail.com>
2020-05-26 21:48:40 -0600

    filters

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Form from "./form";
 import Todo from "./todo";
+import "./index.css";
 
 export default class TodoList extends React.Component {
   state = {
     todos: [],
+    filter: "All",
   };
 
   addTodo = (todo) => {
@@ -29,17 +31,46 @@ export default class TodoList extends React.Component {
     });
   };
 
+  updateFilter = (string) => {
+    this.setState({
+      filter: string,
+    });
+  };
+
   render() {
+    let todos = [];
+
+    if (this.state.filter === "All") {
+      todos = this.state.todos;
+    } else if (this.state.filter === "Active") {
+      todos = this.state.todos.filter((todo) => !todo.complete);
+    } else if (this.state.filter === "Complete") {
+      todos = this.state.todos.filter((todo) => todo.complete);
+    } else {
+      return null;
+    }
+
     return (
-      <div>
+      <div className="App">
         <Form addTodo={this.addTodo} />
-        {this.state.todos.map((todo) => (
+        {todos.map((todo) => (
           <Todo
             key={todo.id}
             todo={todo}
             toggleComplete={() => this.toggleComplete(todo.id)}
           />
         ))}
+        <div>
+          Remaining Todos:{" "}
+          {this.state.todos.filter((todo) => !todo.complete).length}
+        </div>
+        <div>
+          <button onClick={() => this.updateFilter("All")}>All</button>
+          <button onClick={() => this.updateFilter("Active")}>Active</button>
+          <button onClick={() => this.updateFilter("Complete")}>
+            Complete
+          </button>
+        </div>
       </div>
     );
   }
